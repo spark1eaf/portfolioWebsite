ac5e463cb42582432e201f9ec7800b6560e0459d
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2024-10-22 14:46:39 -0500

    added email functionality

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -7,7 +7,7 @@ export const INTRO_BODY = `I'm a Software developer with over two years of exper
 //about
 export const ABOUT_TITLE = `About Me`;
 export const ABOUT_BODY = `I'm a software developer from Lafayette, Louisiana. I discovered my passion for software development while persuing an Electrical Engineering degree at the University of Lafayette Louisiana. I realized about halfway through the program that I preferred programming to calculating the heat of resistors and decided to switch to the university's computer science program.
-As a professional, my past role in the Marine Corps along with my previous career work have taught me to value effective leadership and commmunication above all else. Its my firm belief that organizations excel when leaders have a keen interest in the growth of those in their care. I hope to bring these values with me in any future endeavors. I've always been self motivated when it comes to my work approach and gain great satisfation from finding ways to accomplish tasks more efficiently. 
+As a professional, my past role in the Marine Corps along with my previous career work have taught me to value effective leadership and commmunication above all else. It's my firm belief that organizations excel when leaders have a keen interest in the growth of those in their care. I hope to bring these values with me in any future endeavors. I've always been self motivated when it comes to my work approach and gain great satisfation from finding ways to accomplish tasks more efficiently. 
 As for my hobbies, me and my wife love to travel when time permits. In my free time, you'll probably find me at the gym, playing video games, or hanging out with friends.`;
 
 //contact
@@ -38,3 +38,8 @@ export const PROJECTS_BODY = `Coming soon...`;
 //Routes
 export const HOME_PAGE = `/`;
 export const PROJECTS_PAGE = `projects`;
+
+//email
+export const SERVICE_ID = `service_nljxid9`;
+export const TEMPLATE_ID = `template_j4sxx5b`;
+export const PUBLIC_KEY = `OygQvHW2mwJlfQrH7`;
