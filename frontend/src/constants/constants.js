c8ad87f888f172334b23f2e6e1bbbca60e5330fa
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2024-10-21 22:11:16 -0500

    fixed spelling mistakes

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -2,12 +2,12 @@
 //intro
 export const INTRO_TITLE_CLASS = `intro-title`;
 export const INTRO_TITLE = `Hi, I'm Scott`;
-export const INTRO_BODY = `I'm a Software developer with over two years of expierience. So far, my work has been predominately backend oriented, but I've made it a personal goal to become as well rounded as possible and most recently have been exploring frontend development in my own time. I'm always eager to learn new things and take up new challenges. I created this website to showcase some of the projects I'm working on, so please feel free to take a look around.`;
+export const INTRO_BODY = `I'm a Software developer with over two years of experience. So far my work has been predominately backend oriented, but I've made it a personal goal to become as well rounded as possible and most recently have been exploring frontend development in my own time. I'm always eager to learn new things and take on new challenges. I created this website to showcase some of the projects I'm working on, so please feel free to take a look around.`;
 
 //about
 export const ABOUT_TITLE = `About Me`;
-export const ABOUT_BODY = `I'm a software developer from Lafayette, Louisiana. I discovered my passion for software development while persuing an Electrical Engineering degree at the University of Layayette Louisiana. I realized about halfway through the program that I preferred programming to calculating the heat of resistors and decided to switch to the universities computer science program.
-As a professional, my past role in the Marine Corps along with my previous career work have taught me to value effective leadership and commmunication above all else. Its my firm belief that organizations excel when leaders have a keen interest in the growth of thows in their care. I hope to bring these values with me in any future endeavors. I've always been self motivated when it comes to my work approach and gain great satisfation from finding ways to accomplish tasks more efficiently. 
+export const ABOUT_BODY = `I'm a software developer from Lafayette, Louisiana. I discovered my passion for software development while persuing an Electrical Engineering degree at the University of Layayette Louisiana. I realized about halfway through the program that I preferred programming to calculating the heat of resistors and decided to switch to the university's computer science program.
+As a professional, my past role in the Marine Corps along with my previous career work have taught me to value effective leadership and commmunication above all else. Its my firm belief that organizations excel when leaders have a keen interest in the growth of those in their care. I hope to bring these values with me in any future endeavors. I've always been self motivated when it comes to my work approach and gain great satisfation from finding ways to accomplish tasks more efficiently. 
 As for my hobbies, me and my wife love to travel when time permits. In my free time, you'll probably find me at the gym, playing video games, or hanging out with friends.`;
 
 //contact
