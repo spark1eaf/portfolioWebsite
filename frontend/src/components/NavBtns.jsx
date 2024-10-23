805be59187bde2a0db2eae2ef3d9b047dd2123ba
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2024-10-23 11:51:24 -0500

    adjustment to open Resume in browser instead of prompy download

--- frontend/src/components/NavBtns.jsx
+++ frontend/src/components/NavBtns.jsx
@@ -1,5 +1,6 @@
 import * as Constants from '../constants/constants';
 import { Link } from 'react-router-dom';
+import ResumePDF from '../assets/files/Scott_Hebert_Resume.pdf'
 
 const NavBtns = ({className, navRefs}) =>{
     
@@ -12,24 +13,10 @@ const NavBtns = ({className, navRefs}) =>{
         navRefs.contactRef.current.scrollIntoView({behavior: 'smooth'});
     }
 
-    const promptResumeDownload = () =>{
-        if(window.confirm("Are you sure you want to download this file?")){
-            fetch("src/assets/files/Scott_Hebert_Resume.pdf").then(response =>{
-                response.blob().then(blob =>{
-                    const fileURL = window.URL.createObjectURL(blob);
-                    let alink = document.createElement("a");
-                    alink.href = fileURL;
-                    alink.download="Scott_Hebert_Resume.pdf";
-                    alink.click();
-                })
-            })
-        }
-    }
-
     return(
         <div className= {className}>
             <button onClick={navigateAbout}>About</button>
-            <button onClick={promptResumeDownload}>Resume</button>
+            <a href={ResumePDF} target='_blank'><button>Resume</button></a>
             <Link to={Constants.PROJECTS_PAGE}><button>Projects</button></Link>
             <a href={Constants.GITHUB_LINK} target='_blank'><button>Github</button></a>
             <a href={Constants.LINKEDIN_LINK} target='_blank'><button>Linkedin</button></a>
