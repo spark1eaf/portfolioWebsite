bcb916d1036c6c732d24bc393de4f76533ba518d
Scott Hebert <79615379+sparkleaf@users.noreply.github.com>
2025-02-21 09:36:18 -0600

    resolved grammatical issues

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -5,9 +5,9 @@ export const INTRO_BODY = `I'm a Software developer with over two years of exper
 
 //about
 export const ABOUT_TITLE = `About Me`;
-export const ABOUT_BODY = `I'm a software developer from Lafayette, Louisiana. I discovered my passion for software development while pursuing an Electrical Engineering degree at the University of Lafayette Louisiana. I realized about halfway through the program that I preferred programming to calculating the heat of resistors and decided to switch to the university's computer science program.
-As a professional, my past role in the Marine Corps along with my previous career work have taught me to value effective leadership and communication above all else. It's my firm belief that organizations excel when leaders have a keen interest in the growth of those in their care. I hope to bring these values with me in any future endeavors. I've always been self motivated when it comes to my work approach and gain great satisfaction from finding ways to accomplish tasks more efficiently. 
-As for my hobbies, me and my wife love to travel when time permits. In my free time, you'll probably find me at the gym, playing video games, or hanging out with friends.`;
+export const ABOUT_BODY = `I'm a software developer from Lafayette, Louisiana. I discovered my passion for software development while pursuing an Electrical Engineering degree at the University of Louisiana at Lafayette. I realized about halfway through the program that I preferred programming to calculating the heat of resistors and decided to switch to the university's computer science program.
+As a professional, my past role in the Marine Corps along with my previous career work have taught me to value effective leadership and communication above all else. It's my firm belief that organizations excel when leaders have a keen interest in the growth of those in their care. I hope to bring these values with me in any future endeavors. I've always been self-motivated when it comes to my work approach and gain great satisfaction from finding ways to accomplish tasks more efficiently. 
+As for my hobbies, my wife an I love to travel when time permits. In my free time, you'll probably find me at the gym, playing video games, or hanging out with friends.`;
 
 //contact
 export const CONTACT_CLASS = `contact`;
